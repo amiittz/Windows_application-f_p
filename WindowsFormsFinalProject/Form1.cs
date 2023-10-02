5e13e44d800d143561fc4f418653788335858bea
amiittz <amitgtima@gmail.com>
2023-10-02 12:58:04 +0300

    add the start

---
+++ WindowsFormsFinalProject/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsFinalProject
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Wall w1 = new Wall(50, 50, 50, 300);
+            w1.Draw(g);
+        }
+    }
+}
---
+++ WindowsFormsFinalProject/Wall.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+
+public class Wall
+{
+    private Rectangle Bounds { get; set; }
+    private Image WallImage;
+
+    public Wall(int x, int y, int width, int height)
+    {
+        Bounds = new Rectangle(x, y, width, height);
+    }
+
+    public bool CollidesWith(Rectangle otherBounds)
+    {
+        return Bounds.IntersectsWith(otherBounds);
+    }
+
+    public void Draw(Graphics graphics)
+    {
+        if(Bounds.Width > Bounds.Height)
+        {
+            WallImage = Image.FromFile(@"pics\wallHorizontal.jpg");
+        }
+        else
+        {
+            WallImage = Image.FromFile(@"pics\wallVertical.jpg");
+        }
+        
+        graphics.DrawImage(WallImage, Bounds);
+        
+    }
+
+}
